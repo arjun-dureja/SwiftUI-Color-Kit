d608120fea258eb1606989413d6e5750303f9755
Arjun Dureja <arjun.dureja1000@gmail.com>
2023-11-18 23:25:54 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ColorKit",
-            dependencies: ["Sliders"]),
+            dependencies: ["Sliders-SwiftUI"]),
         .testTarget(
             name: "ColorKitTests",
             dependencies: ["ColorKit"]),
