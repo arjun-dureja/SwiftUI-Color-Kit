9b39d0653882dece2c6eb9cc70bf86acdb63c5f9
Arjun Dureja <arjun.dureja1000@gmail.com>
2023-11-19 00:00:44 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ColorKit",
-    platforms: [.iOS(.v13), .macOS(.v11)],
+    platforms: [.iOS(.v13), .macOS(.v10_16)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
